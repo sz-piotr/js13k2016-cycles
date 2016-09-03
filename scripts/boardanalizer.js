5f2d64e9e087ea5f9961f8ae0be3f49f88d53568
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2016-09-03 13:32:20 +0200

    board generation partially working

--- scripts/boardanalizer.js
+++ scripts/boardanalizer.js
@@ -1,5 +1,45 @@
 let BoardAnalizer = {
     isTree: function(board) {
-
+        let visited = this.createVisitedArray(board.length);
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++) {
+                if (visited[i][j] === false) {
+                    if (this.visit(board, visited, i, j))
+                        return false;
+                }
+            }
+        }
+        return true;
+    },
+    visit: function(board, visited, x, y, direction) {
+        visited[x][y] = true;
+        if (direction !== 'n' && board[x][y].n && board[x][y + 1] && board[x][y + 1].s) {
+            if (this.visit(board, visited, x, y + 1, 's'))
+                return true;
+        }
+        if (direction !== 'e' && board[x][y].e && board[x + 1] && board[x + 1][y].w) {
+            if (this.visit(board, visited, x + 1, y, 'w'))
+                return true;
+        }
+        if (direction !== 's' && board[x][y].s && board[x][y - 1] && board[x][y - 1].n) {
+            if (this.visit(board, visited, x, y - 1, 'n'))
+                return true;
+        }
+        if (direction !== 'w' && board[x][y].w && board[x - 1] && board[x - 1][y].e) {
+            if (this.visit(board, visited, x - 1, y, 'e'))
+                return true;
+        }
+        return false;
+    },
+    createVisitedArray: function(size) {
+        let visited = [];
+        for (let i = 0; i < size; i++) {
+            let row = [];
+            for (let i = 0; i < size; i++) {
+                row.push(false);
+            }
+            visited.push(row);
+        }
+        return visited;
     }
 }
--- scripts/boardcreator.js
+++ scripts/boardcreator.js
@@ -1,6 +1,7 @@
 let BoardCreator = {
     create: function() {
         let board = this.init();
+        this.spiralFill(board);
         return board;
     },
     init: function() {
@@ -9,16 +10,71 @@ let BoardCreator = {
             let row = [];
             for (let j = 0; j < 7; j++) {
                 if (i % 2 === j % 2) {
-                    row.push(this.randomTile());
-                } else {
                     row.push({});
+                } else {
+                    row.push(this.randomTile());
                 }
             }
             board.push(row);
         }
         return board;
     },
+    spiralFill: function(board) {
+        let position = {
+            x: 3,
+            y: 3
+        };
+        let step = 1;
+        let steps = 2;
+        let direction = 0;
+        while (inBounds(position)) {
+            for (let i = 0; i < step; i++) {
+                fill(board, position);
+                position.x += this.directions[direction].x;
+                position.y += this.directions[direction].y;
+            }
+            direction = (direction + 1) % 4;
+            steps--;
+            if (steps === 0) {
+                step++;
+                steps = 2;
+            }
+        }
+
+        function fill(board, position) {
+            if (isEmpty(board[position.x][position.y])) {
+                let tiles = tileset.slice();
+                Utils.shuffle(tiles);
+                while (true) {
+                    board[position.x][position.y] = tiles.pop();
+                    if (BoardAnalizer.isTree(board))
+                        break;
+                }
+            }
+        }
+
+        function inBounds(position) {
+            return position.x >= 0 && position.x < 7 && position.y >= 0 && position.y < 7;
+        }
+
+        function isEmpty(tile) {
+            return !(tile.hasOwnProperty('n') || tile.hasOwnProperty('e') || tile.hasOwnProperty('s') || tile.hasOwnProperty('w'));
+        }
+    },
     randomTile: function() {
         return tileset[Math.floor(Math.random() * tileset.length)];
-    }
+    },
+    directions: [{
+        x: 0,
+        y: 1
+    }, {
+        x: 1,
+        y: 0
+    }, {
+        x: 0,
+        y: -1
+    }, {
+        x: -1,
+        y: 0
+    }]
 }
