098d2a6c4ad204c94241e5de8130957f4a96e870
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2016-09-10 17:41:42 +0200

    more levels!!!

--- scripts/levels.js
+++ scripts/levels.js
@@ -22,26 +22,92 @@ let Levels = [{
     objectiveMet: noGlitch
 }, {
     isGlitch: function(position) {
+        return (position.x === position.y) && (position.x === 2 || position.x === 4);
+    },
+    text: 'The glitch is spreading, stop it!',
+    time: 240,
+    objectiveMet: noGlitch
+}, {
+    isGlitch: function(position) {
         return (position.x === 2 || position.x === 4) && (position.y === 2 || position.y === 4);
     },
-    time: 10,
+    turns: 200,
     text: 'Let no glitch survive!',
     objectiveMet: noGlitch
 }, {
     isGlitch: function(position) {
         return position.x + 1 === position.y + 1;
     },
-    turns: 100,
-    text: 'Put an end to this corruption',
+    turns: 150,
+    text: 'Don\'t let the glitch overwhelm you',
     objectiveMet: noGlitch
 }, {
     isGlitch: function(position) {
         return position.y === 3 || position.x === 3;
     },
-    time: 240,
-    text: 'Score 10,000 points',
+    time: 200,
+    text: 'Score 5,000 points',
     objectiveMet: function(data) {
-        return data.score >= 10000;
+        return data.score >= 5000;
+    }
+}, {
+    isGlitch: function(position) {
+        switch (position.x) {
+            case 1:
+            case 5:
+                return position.y === 3;
+            case 2:
+            case 4:
+                return position.y === 2 || position.y === 4;
+            case 3:
+                return position.y === 1 || position.y === 5;
+            default:
+                return false;
+        }
+    },
+    turns: 40,
+    text: 'Get a x5 combo',
+    objectiveMet: function(data) {
+        return data.combo.current >= 5;
+    }
+}, {
+    isGlitch: function(position) {
+        switch (position.y) {
+            case 0:
+            case 5:
+                return position.x === 0 || position.x === 6;
+            case 2:
+            case 3:
+                return position.x % 3 !== 0;
+            case 4:
+                return position.x === 3;
+            case 6:
+                return position.x % 2 === 0;
+            default:
+                return false;
+        }
+    },
+    turns: 160,
+    text: 'Stop the evil glitch!!!',
+    objectiveMet: noGlitch
+}, {
+    isGlitch: function(position) {
+        return position.x % 3 === position.y % 3;
+    },
+    turns: 20,
+    text: 'Score 1,500 points',
+    objectiveMet: function (data) {
+        return data.score >= 1500;
+    }
+}, {
+    isGlitch: function(position) {
+        return position.x % 3 === 0;
+    },
+    time: 360,
+    text: 'Score 1,000 points in one turn',
+    objectiveMet: function (data) {
+        this.lastScore = this.lastScore || 0;
+        return data.score - this.lastScore >= 1000;
     }
 }, {
     isGlitch: function(position) {
